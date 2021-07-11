962c3e65df240a652688ca6a92b5598b26dc254b
unknown <nadim.dipu@northsouth.edu>
2021-07-11 23:28:49 +0600

    Added the baseUrl in the service file and also created the function to fetch the API data.

--- angular-api-search-application/src/app/data-serve.service.ts
+++ angular-api-search-application/src/app/data-serve.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServeService {
+  root_url = "https://dev.gozayaan.com/api/gozayaan_campaign/real_estate_list/?city_name=";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  test_api(city_name: string) {
+    const tree_data = this.http.get(`${this.root_url}${city_name}`);
+
+    return tree_data;
+  }
 }
