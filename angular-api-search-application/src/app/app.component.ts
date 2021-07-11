79ec734a5ccde1f494b939d341791a4314abde9c
unknown <nadim.dipu@northsouth.edu>
2021-07-11 23:38:32 +0600

    Created the function to use the data-serve service to get the data from the API to the app.component.ts file.

--- angular-api-search-application/src/app/app.component.ts
+++ angular-api-search-application/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DataServeService} from "./data-serve.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-api-search-application';
+  title = 'real-estate-api';
+  result = null;
+  message = null;
+
+  constructor(private dataService: DataServeService) {
+  }
+
+  showResults(city_name: string) {
+    if (city_name.length === 0) {
+      this.result = null;
+      this.message = "Please enter search term in the search bar.";
+      return;
+    }
+    try {
+      this.dataService.test_api(city_name).subscribe((data) => {
+        this.result = data["result"][0];
+        if(this.result == null) {
+          this.message = "Sorry, we couldn't find anything matching that query.";
+        } else {
+          this.message = null;
+        }
+      });
+    } catch (error) {
+      this.message = "Some error has occured! Please try again."
+    }
+
+  }
+
 }
